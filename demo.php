84900ada20a436575666e6a8e229680cebf930d1
Erlend Mongstad <eeerlend@gmail.com>
2012-12-08 15:45:50 +0100

    Removed POST call by default

--- demo.php
+++ demo.php
@@ -1,18 +1,13 @@
 <html>
 <body>
-<?php require_once('TeamLab.php'); ?>
-
 <?php
 try {
-    // TeamLab::logout();
-    
+    require_once('TeamLab.php');
     TeamLab::initWith(
         'USERNAME', // username (email)
         'PASSWORD', // password
-        'PORALNAME.teamlab.com' // <portalname>.teamlab.com
+        'PORAL.teamlab.com' // <portalname>.teamlab.com
         );
-    
-    
     /* 
      * GET
      * Example: http://api.teamlab.com/docs/people/get/api/1.0/people
@@ -31,19 +26,25 @@ try {
      * POST
      * Example: http://api.teamlab.com/docs/crm/get/api/1.0/crm/contact
      */
-     
-    // Creates a new crm-contact in your portal
-    $newPerson = TeamLab::post('api/1.0/crm/contact/person', array(
+         
+    // Uncomment this to create a contact
+    /* $newPerson = TeamLab::post('api/1.0/crm/contact/person', array(
         'isPrivate' => 'true',
         'firstName' => 'John',
         'lastName' => 'Doe',
-    ));
+    )); */
+    
+    echo '<h1>Example POST call</h1>';
     
     // Dump the data
-    echo '<h1>Example POST call</h1>';
-    echo '<pre>';
-    var_dump($newPerson);
-    echo '</pre>';
+    if (is_object($newPerson)) {
+        echo '<pre>';
+        var_dump($newPerson);
+        echo '</pre>';
+    } else {
+        echo '<p>Uncomment the POST-call in demo.php to actually create a contact in your portal</p>';
+    }
+    
     
 } catch (Exception $e) {
     echo '<h1></h1>Caught exception:<br />',  $e->getMessage(), "\n";
